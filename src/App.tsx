4b7cdc18cef3093eaac1c915e8ff32f06b16e73c
mariuszsak <mariuszsak@gmail.com>
2021-07-19 17:01:27 +0200

    Error handler added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import './App.scss';
 import {Song} from './util/type';
-import fetchSong from './service/FetchSong';
+import fetchSongs from './service/FetchSongs';
 import Loader from './components/Loader';
 import Player from './components/Player';
+import Error from './components/Error';
 
 const App = () => {
     const [allSongs, setAllSongs] = useState<Song[]>([]);
     const [isListLoaded, setIsListLoaded] = useState<boolean>(false);
+    const [hasError, setHasError] = useState<boolean>(false);
 
-    useEffect((): void => {
-        fetchSong().then((result: Song[]) => {
+    useEffect(() => {
+        fetchSongs().then((result: Song[]) => {
             setAllSongs(result);
             setIsListLoaded(true)
+        }).catch(error => {
+            setHasError(true);
+            console.error(error);
         });
     }, [])
 
     return (
-        isListLoaded ? <Player songs={allSongs}/> : <Loader/>
+        <>
+            {hasError && <Error/>}
+            {isListLoaded ? <Player songs={allSongs}/> : <Loader/>}
+        </>
     );
 }
 
