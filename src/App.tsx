0df4c86819ca44fe98bef5a582a8baba40ce6834
mariuszsak <mariuszsak@gmail.com>
2021-07-19 14:02:48 +0200

    State variable name changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import Loader from './components/Loader';
 import Player from './components/Player';
 
 const App = () => {
-    const [songs, setSongs] = useState<Song[]>([]);
+    const [allSongs, setAllSongs] = useState<Song[]>([]);
     const [isListLoaded, setIsListLoaded] = useState<boolean>(false);
 
     useEffect((): void => {
         fetchSong().then((result: Song[]) => {
-            setSongs(result);
+            setAllSongs(result);
             setIsListLoaded(true)
         });
     }, [])
 
     return (
-        isListLoaded ? <Player songs={songs}/> : <Loader/>
+        isListLoaded ? <Player songs={allSongs}/> : <Loader/>
     );
 }
 
