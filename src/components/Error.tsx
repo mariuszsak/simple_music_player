bc849fab0e03003ecd5fd812ee22315b7fe1ccef
mariuszsak <mariuszsak@gmail.com>
2021-07-19 17:01:58 +0200

    Error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css';
+
+const Error = () => {
+    return (
+        <div className='error'>
+            <div>Something went bad :(</div>
+        </div>
+    );
+};
+
+export default Error;
